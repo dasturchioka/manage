a05ffb86f0e701513090bc690dec396e2f0c520a
dasturchioka <dasturchioka@gmail.com>
2023-07-11 03:09:05 +0500

    client: implemented additional designs. Functionalities will be soon

--- client/src/components/TheTableOfTasks.vue
+++ client/src/components/TheTableOfTasks.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { onClickOutside } from "@vueuse/core";
 import { TASK_STATUS } from "@/constants/index";
+import TheTableSort from "./TheTableSort.vue";
 import TheStatus from "./TheStatus.vue";
 
 defineProps({
@@ -16,9 +17,14 @@ defineProps({
 });
 
 const showForm = ref(false);
+const showTable = ref(true);
 
 const handleForm = () => {
   showForm.value = !showForm.value;
+};
+
+const handleTable = () => {
+  showTable.value = !showTable.value;
 };
 
 const outsideClickTarget = ref(null);
@@ -32,20 +38,42 @@ onClickOutside(outsideClickTarget, () => {
   <div
     class="list-of-tasks bg-dark-secondary sm:p-6 p-4 rounded w-full relative overflow-x-scroll custom-scroll-h"
   >
-    <h1
-      class="flex items-center text-2xl font-bold sm:mb-6 mb-4 first-letter:uppercase"
-    >
-      <img
-        src="../assets/icons/dashboard_icon.svg"
-        class="mr-2"
-        alt="manage"
-      />{{ dashboard }}
-    </h1>
+    <div class="top flex items-center justify-between">
+      <div class="top-left flex items-center">
+        <button
+          @click="handleTable"
+          class="title flex items-center text-2xl font-bold first-letter:uppercase"
+        >
+          <img
+            src="../assets/icons/dashboard_icon.svg"
+            class="mr-2"
+            alt="manage"
+          />{{ dashboard }}
+          <img
+            v-if="!showTable"
+            class="ml-3"
+            src="../assets/icons/arrow-right.svg"
+            alt="arrow right "
+          />
+          <img
+            v-else
+            class="ml-3"
+            src="../assets/icons/arrow-bottom.svg"
+            alt="arrow bottom "
+          />
+        </button>
+        <p class="ml-4 bg-dark text-gray-700 rounded text-sm font-extrabold uppercase py-1 px-2" v-if="!showTable">7 tasks</p>
+      </div>
+      <div class="top-right">
+        <TheTableSort />
+      </div>
+    </div>
 
-    <div class="relative overflow-x-auto custom-scroll-h custom-scroll">
-      <table
-        class="w-full text-sm text-left text-gray-500 dark:text-gray-400 "
-      >
+    <div
+      v-show="showTable"
+      class="relative sm:mt-6 mt-4 overflow-x-auto custom-scroll-h custom-scroll"
+    >
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <tbody class="mt-4 space-y-4">
           <tr class="bg-dark">
             <th
@@ -146,6 +174,7 @@ onClickOutside(outsideClickTarget, () => {
         </tbody>
       </table>
     </div>
+
     <!-- <form v-else class="not-found overflow-x-auto w-full min-w-[548px]">
       <p class="not-found">You don't have any task in this dashboard</p>
       <div ref="outsideClickTarget" class="w-full">
---
+++ client/src/components/TheTableSort.vue
@@ -0,0 +1,63 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import { onClickOutside } from "@vueuse/core";
+
+const showOptions = ref(false);
+
+const handleOptions = () => {
+  showOptions.value = !showOptions.value;
+};
+
+const outsideClickTarget = ref(null);
+
+onClickOutside(outsideClickTarget, () => {
+  showOptions.value = false;
+});
+</script>
+
+<template>
+  <div class="sort-component relative">
+    <button
+      @click="handleOptions"
+      class="component-sort rounded p-3 transition hover:bg-dark-lighter"
+    >
+      <img src="../assets/icons/sort.svg" alt="manage" />
+    </button>
+    <div
+      v-show="showOptions"
+      ref="outsideClickTarget"
+      class="status-list flex max-h-[120px] overflow-y-scroll custom-scroll absolute right-0 items-center z-[99999999999] flex-col min-w-[200px] bg-dark break-words rounded border border-gray-700"
+    >
+      <button
+        class="list-btn transition hover:bg-dark-lighter disabled:hover:bg-none uppercase flex items-center font-extrabold px-3 py-2 w-full disabled:opacity-20 text-sm"
+      >
+        High priorities up
+      </button>
+      <button
+        class="list-btn transition hover:bg-dark-lighter disabled:hover:bg-none uppercase flex items-center font-extrabold px-3 py-2 w-full disabled:opacity-20 text-sm"
+      >
+        Low priorities up
+      </button>
+      <button
+        class="list-btn transition hover:bg-dark-lighter disabled:hover:bg-none uppercase flex items-center font-extrabold px-3 py-2 w-full disabled:opacity-20 text-sm"
+      >
+        todo up
+      </button>
+      <button
+        class="list-btn transition hover:bg-dark-lighter disabled:hover:bg-none uppercase flex items-center font-extrabold px-3 py-2 w-full disabled:opacity-20 text-sm"
+      >
+        in progress up
+      </button>
+      <button
+        class="list-btn transition hover:bg-dark-lighter disabled:hover:bg-none uppercase flex items-center font-extrabold px-3 py-2 w-full disabled:opacity-20 text-sm"
+      >
+        completed up
+      </button>
+      <button
+        class="list-btn transition hover:bg-dark-lighter disabled:hover:bg-none uppercase flex items-center font-extrabold px-3 py-2 w-full disabled:opacity-20 text-sm"
+      >
+        failed up
+      </button>
+    </div>
+  </div>
+</template>
