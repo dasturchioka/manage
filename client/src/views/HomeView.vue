c01e48c973b566ed9c2204b3ff934c1b67a7a5d9
dasturchioka <dasturchioka@gmail.com>
2023-07-10 17:36:34 +0500

    client: working with overview page design

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
-import TheHorizontalScroll from "@/components/TheHorizontalScroll.vue";
+import TheListOfTasks from '@/components/TheListOfTasks.vue';
 </script>
 
 <template>
   <main class="overview-page sm:px-4 px-2 space-y-12 w-full">
-    <TheHorizontalScroll>
-      <template #titles>
-        <h1>Overview</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit ex
-          distinctio quaerat
-        </p>
-      </template>
-      <template #content>
-        <div
-          class="flex-shrink-0 min-h-56 px-5 py-4 bg-white rounded shadow-sm default-block w-80"
-        ></div>
-      </template>
-    </TheHorizontalScroll>
+    <div class="titles">
+      <h1>Overview</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit ex
+        distinctio quaerat
+      </p>
+    </div>
+    <TheListOfTasks/>
   </main>
 </template>
 
