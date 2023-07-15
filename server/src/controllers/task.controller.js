9e1b1dbfc3644601cce2e8ce80c052af04950715
dasturchioka <dasturchioka@gmail.com>
2023-07-15 14:21:17 +0500

    server: status and priority properties changed to ints

--- server/src/controllers/task.controller.js
+++ server/src/controllers/task.controller.js
@@ -3,15 +3,32 @@ const prisma = new PrismaClient();
 
 const getAllTasks = async (req, res) => {
   try {
-    const { sortBy } = req.query;
+    const tasks = await prisma.tasks.findMany({
+      orderBy: {
+        dashboardId: "asc",
+      },
+    });
+
+    if (!tasks) {
+      return res.status(404).json({
+        status: "not found",
+        msg: "There are no tasks yet.",
+      });
+    }
+
+    return res.json({ tasks, status: "ok" });
+  } catch (error) {
+    return res.status(500).json({ error, msg: error.message });
+  }
+};
+
+const getDashboardTasks = async (req, res) => {
+  try {
     const { id: dashboardId } = req.body;
 
     const tasks = await prisma.tasks.findMany({
       where: {
         dashboardId,
-      },
-      orderBy: {
-        dashboardId: "",
       },
     });
 
@@ -21,11 +38,7 @@ const getAllTasks = async (req, res) => {
         msg: "There are no tasks in this dashboard yet.",
       });
     }
-
-    return res.json({ tasks, status: "ok" });
-  } catch (error) {
-    return res.status(500).json({ error, msg: error.message });
-  }
+  } catch (error) {}
 };
 
 module.exports = { getAllTasks };
